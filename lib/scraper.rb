0ad1e5ab2890dd01792ab4bf1068aa4839c4fdaa
MuhammadDanyalMahdi <danyalm50@gmail.com>
2021-03-06 15:52:08 -0500

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri:HTML(html)
+doc.css(".site-header__hero__headline").text.strip
+courses = doc.css(."title-oE5vT4")
+
+
+courses.each do |course|
+    puts course.text.strip
+  end 
